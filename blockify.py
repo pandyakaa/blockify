135fc4a23839ea54bbaa1eb419ea3217f84dbdc8
Pandyaka Aptanagi <pandyaka.aptanagi@gmail.com>
2020-10-22 11:40:24 +0700

    add decrypt

--- blockify.py
+++ blockify.py
@@ -12,11 +12,29 @@ def feistel_function(round_key: str, partial_block: str) -> str:
 
 
 def encrypt_per_block(block: str, keys: list) -> str:
-    pass
+    left_block, right_block = split_block(block)
+    for i in range(ROUNDS):
+        feistel_function_result = feistel_function(keys[i], right_block)
+        temp = xor(left_block, feistel_function_result)
+
+        left_block = right_block
+        right_block = temp
+    
+    encrypted_text_per_block = left_block + right_block
+    return encrypted_text_per_block
 
 
 def decrypt_per_block(block: str, keys: list) -> str:
-    pass
+    left_block, right_block = split_block(block)
+    for i in range(ROUNDS-1, -1 , -1):
+        feistel_function_result = feistel_function(keys[i], left_block)
+        temp = xor(right_block, feistel_function_result)
+
+        right_block = left_block
+        left_block = temp
+    
+    decrypted_text_per_block = left_block + right_block
+    return decrypted_text_per_block
 
 
 def encrypt(plain_text: str, key: str, mode: str) -> str:
@@ -36,15 +54,34 @@ def encrypt(plain_text: str, key: str, mode: str) -> str:
             encrypted_text += encrypted_block
             iv = encrypted_block
         else:
-            encrypted_block = encrypt_per_block(block, keys)
-            encrypted_text += xor(encrypted_block, counter_block[i])
+            encrypted_block = encrypt_per_block(counter_block[i], keys)
+            encrypted_text += xor(encrypted_block, block)
 
     return encrypted_text
 
 
 def decrypt(cipher_text: str, key: str, mode: str) -> str:
-    # kebalikan dari encrypt
-    pass
+    text_blocks = text_to_block(cipher_text)
+    keys = generate_key(key, ROUNDS)
+    iv = '0' * BLOCK_SIZE
+    counter_block = initiate_counter_block(ROUNDS)
+
+    decrypted_text = ''
+    for i in range(len(text_blocks)):
+        block = text_blocks[i]
+        if mode == 'ecb':
+            decrypted_text += decrypt_per_block(block, keys)
+        elif mode == 'cbc':
+            decrypted_block = decrypt_per_block(block, keys)
+            decrypted_block = xor(decrypted_block, iv)
+            decrypted_text += decrypted_block
+            iv = block
+        else:
+            decrypted_block = decrypt_per_block(counter_block[i], keys)
+            decrypted_text += xor(decrypted_block, block)
+
+    return decrypted_text
+
 
 
 if __name__ == "__main__":
