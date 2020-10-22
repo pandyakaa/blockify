5ba956864a9196c32d18cd72a21736e8f4c98a40
Pandyaka Aptanagi <pandyaka.aptanagi@gmail.com>
2020-10-22 21:43:58 +0700

    finished

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 if __name__ == "__main__":
 
     ecb_enc_start_time = time.time()
-    enc_tc = encrypt('pandyaka aptanagi', 'ini adalah kunci', 'ecb')
+    enc_tc = encrypt('pandyakaaptanagi', 'ini adalah kunci', 'ecb')
     ecb_enc_end_time = time.time()
     print('ecb time {}'.format(ecb_enc_end_time - ecb_enc_start_time))
 
@@ -18,30 +18,30 @@ if __name__ == "__main__":
 
     print('===============================================================')
 
-    # cbc_enc_start_time = time.time()
-    # enc_tc = encrypt('pandyaka aptanagi', 'ini adalah kunci', 'cbc')
-    # cbc_enc_end_time = time.time()
-    # print('cbc time {}'.format(cbc_enc_end_time - cbc_enc_start_time))
+    cbc_enc_start_time = time.time()
+    enc_tc = encrypt('pandyakaaptanagi', 'ini adalah kunci', 'cbc')
+    cbc_enc_end_time = time.time()
+    print('cbc time {}'.format(cbc_enc_end_time - cbc_enc_start_time))
 
-    # cbc_dec_start_time = time.time()
-    # dec_tc = decrypt(enc_tc, 'ini adalah kunci', 'cbc')
-    # cbc_dec_end_time = time.time()
-    # print('cbc time {}'.format(cbc_dec_end_time - cbc_dec_start_time))
+    cbc_dec_start_time = time.time()
+    dec_tc = decrypt(enc_tc, 'ini adalah kunci', 'cbc')
+    cbc_dec_end_time = time.time()
+    print('cbc time {}'.format(cbc_dec_end_time - cbc_dec_start_time))
 
-    # print('cbc encryption result: {}'.format(enc_tc))
-    # print('cbc decryption result: {}'.format(dec_tc))
+    print('cbc encryption result: {}'.format(enc_tc))
+    print('cbc decryption result: {}'.format(dec_tc))
 
-    # print('===============================================================')
+    print('===============================================================')
 
-    # counter_enc_start_time = time.time()
-    # enc_tc = encrypt('pandyaka aptanagi', 'aptanagi', 'counter')
-    # counter_enc_end_time = time.time()
-    # print('counter time {}'.format(counter_enc_end_time - counter_enc_start_time))
+    counter_enc_start_time = time.time()
+    enc_tc = encrypt('pandyakaaptanagi', 'aptanagi', 'counter')
+    counter_enc_end_time = time.time()
+    print('counter time {}'.format(counter_enc_end_time - counter_enc_start_time))
 
-    # counter_dec_start_time = time.time()
-    # dec_tc = decrypt(enc_tc, 'aptanagi', 'counter')
-    # counter_dec_end_time = time.time()
-    # print('counter time {}'.format(counter_dec_end_time - counter_dec_start_time))
+    counter_dec_start_time = time.time()
+    dec_tc = decrypt(enc_tc, 'aptanagi', 'counter')
+    counter_dec_end_time = time.time()
+    print('counter time {}'.format(counter_dec_end_time - counter_dec_start_time))
 
-    # print('counter encryption result: {}'.format(enc_tc))
-    # print('counter decryption result: {}'.format(dec_tc))
+    print('counter encryption result: {}'.format(enc_tc))
+    print('counter decryption result: {}'.format(dec_tc))
